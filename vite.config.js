06b54e22e2cd663bbd56c11620befb2e157f06cf
KauaMouraa <kauamoura.dev@gmail.com>
2025-11-28 09:32:40 -0300

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Enem-quiz-with-React/', // ← IMPORTANTE!
+  base: '/Enem-quiz-with-React/',
 })
