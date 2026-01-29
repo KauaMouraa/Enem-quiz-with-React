d8bc0f4d4e3a6d79d9e0b78f4039e3e9801f90b8
KauaMouraa <kauamoura.dev@gmail.com>
2026-01-29 14:11:22 +0000

    Add pages

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+const About = () => {
+    return ( 
+        <>
+             <header className="">
+                <div className="">               
+                    <h1 className="text-2xl font-bold">Sobre</h1>
+                    <p>
+                        
+                    </p>
+                </div>
+            </header>
+        </>
+     );
+}
+ 
+export default About;
